b6eafea2ad029d75e03a694980e188eb894a2879
EslamSaloka <eslammohsenhandousa@gmail.com>
2019-10-22 13:56:10 +0200

    add

--- src/Common/View/Dashboard/layouts/menu.blade.php
+++ src/Common/View/Dashboard/layouts/menu.blade.php
@@ -1,7 +1,7 @@
 <div class="sidebar-category sidebar-category-visible">
     <div class="category-content no-padding">
         <ul class="navigation navigation-main navigation-accordion">
-            <li class="navigation-header"><span>@lang('Main')</span> <i class="icon-menu" title="Main pages"></i></li>
+            <li class="navigation-header"><input type="text" placeholder="@lang('Search in menu')"></li>
             <li><a href="{{ route('dashboard.Dindex') }}"><i class="icon-home4"></i> <span>@lang('Dashboard')</span></a></li>
             {!! AdminMenu() !!}
         </ul>
