b67e935f892777430ab02f1ae7bf26a6736f4b44
EslamSaloka <35349478+EslamSaloka@users.noreply.github.com>
2020-07-06 10:50:41 +0200

    Update Index.php

--- src/Common/Controller/Front/Index.php
+++ src/Common/Controller/Front/Index.php
@@ -1,16 +1,13 @@
 <?php
 
-namespace App\Components\Common\Controller\Front;
+namespace Tasawk\TasawkComponent\Common\Controller\Dashboard;
 
-use App\Components\Controller;
-use App\Components\Product\Model\Product;
+use Tasawk\TasawkComponent\Controller;
 
 class Index extends Controller {
 
     public function __invoke() {
-        // $product = Product::paginate(2);
-        $product = Product::regular()->with('translations')->get();
-        return view('this::home', get_defined_vars());
+
     }
 
 }
